5564a55bad5bc644d0692b69243d489591ba4646
Chris Andrew <81700566+cha519@users.noreply.github.com>
2021-11-25 18:21:11 +0000

    Add intial app skeleton

---
+++ ConsumeDiscogsAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConsumeDiscogsAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
