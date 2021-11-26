096ec47a71a1df04b1c1c54d1254b3ad05a4200e
Chris Andrew <81700566+cha519@users.noreply.github.com>
2021-11-26 13:05:07 +0000

    JSON  now queryable through LINQ via JObject.

--- ConsumeDiscogsAPI/Controllers/HomeController.cs
+++ ConsumeDiscogsAPI/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using ConsumeDiscogsAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -23,25 +24,39 @@ namespace ConsumeDiscogsAPI.Controllers
         public async Task<IActionResult> Index()
         {
             string username = "chrishughandrew";
+            JObject wantlist = new();
             using (HttpClient httpClient = new())
             {
-                //httpClient.BaseAddress = new Uri("https://api.discogs.com/");
                 httpClient.BaseAddress = new Uri("https://api.discogs.com/");
 
                 //discogs requires a User Agent string
-                string userAgentString = "ConsumeDiscogsAPI/1.0"; //TODO: Discogs wants it in this format "ConsumeDiscogsAPI/1.0" +https://localhost:44309/"; but .NET wont parse it.
+                string userAgentString = "ConsumeDiscogsAPI/1.0"; //TODO: Discogs wants it in this format "ConsumeDiscogsAPI/1.0" +https://localhost:44309/"; but .NET wont parse it. Works for now.
                 httpClient.DefaultRequestHeaders.Add("User-Agent", userAgentString);
                 httpClient.DefaultRequestHeaders.Accept.Clear();
                 httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 HttpResponseMessage response = await httpClient.GetAsync($"users/{username}/wants");
-                //HttpResponseMessage response = await httpClient.GetAsync($"api/character/5");
 
-                if (response.IsSuccessStatusCode)
+                if (!response.IsSuccessStatusCode)
                 {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    //give error view if fails to get data
+                    return RedirectToAction(nameof(Error));
                 }
+
+                // get the content and convert to a LINQ queryable object
+                string responseContent = await response.Content.ReadAsStringAsync();
+                wantlist = JObject.Parse(responseContent);
             }
 
+                // select out pertient data from wantlist: id, title, artist, label:name catno; genre, style 
+                JArray allWants = (JArray)wantlist["wants"];
+                
+
+                // Map each to a Want Class
+
+                // Add to Wants list
+
+
+               
             return View();
         }
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ConsumeDiscogsAPI
-A simple .NET Core MVC Web Application for demonstrating the consumption of the Discogs API
+A simple .NET Core MVC Web Application for demonstrating the consumption of the Discogs API.
+
+Discogs API Documentation found [here](https://www.discogs.com/developers/).
+
+## Quirks/Concerns/TODO
+- Discogs API requests that the developer provide a specifc format for the User-Agent HTTP header string (required or the request is 403 Forbidden). .NET wouldn't parse this full string so a partial version of that header is provided and currently works. Discogs warns that it may quietly block apps who don't provide the full header.   
